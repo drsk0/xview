711de029c7bccc4c1de6982d27873eedf31d3f90
drsk <drsk@lavabit.com>
2021-05-25 21:47:02 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Julia Development Environment
+=============================
+
+A nix flake for Julia development. This uses the FHS provided by
+www.github.com:olynch/scientific-fhs.git.
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Julia development environment";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; allowBroken = true; permittedInsecurePackages =
+        ["libgit2-0.27.10"]; };
+        # overlays = [ self.overlay ];
+      };
+
+
+      fhsCommand = pkgs.callPackage ./scientific-fhs {
+          juliaVersion = "julia_16";
+      };
+    in
+    {
+
+      devShell.${system} = pkgs.mkShell rec {
+        buildInputs = [
+          (fhsCommand "julia" "julia")
+          (fhsCommand "julia-bash" "bash")
+        ];
+
+        # CPATH = pkgs.lib.makeSearchPathOutput "dev" "include" buildInputs;
+        # shellHook = ''
+        # '';
+      };
+    };
+}
