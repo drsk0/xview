4abd94f89e4922d071687e23b927370ae1f12b65
drsk <drsk@lavabit.com>
2021-11-23 12:54:22 +0100

    implemented local rotations for sampling tiles.

--- src/Tiles.jl
+++ src/Tiles.jl
@@ -7,6 +7,9 @@ using GeoArrays
 using DataFrames
 using Geodesy
 using Pipe
+using Rotations
+using ImageTransformations
+using CoordinateTransformations
 
 const TileCollection = Array{GeoArray,1}
 
@@ -34,26 +37,60 @@ function generateTiles(width::Int, overlap::Int, ga::GeoArray)::TileCollection
   return tiles
 end
 
-"For a given tile, generate the image of the distribution given the ground truth data."
-function generateImageTile(ga::GeoArray, df::DataFrame) ::Array{Float32, 2}
-  "get pixels for fishing vessels in tile and set them to one."
+"Find pixels of vessels in given GeoArray"
+function allVessels(ga::GeoArray, df::DataFrame)::Array{(Int, Int),1}
   (x_max, y_max, _bands) = size(ga.A)
   vessels = @pipe [(d.detect_lat, d.detect_lon, 0.0) for d in eachrow(df[(df.is_vessel .!== missing) .& (df.is_vessel .== true), :])] .|>
               LLA(_ ...) .|>
               UTMZfromLLA(wgs84) .|>
-              indices(ga, (_.x, _.y)) |>
-              filter(xy -> 1 <= xy[1] && xy[1] <= x_max && 1 <= xy[2] && xy[2] <= y_max, _)
+              indices(ga, (_.x, _.y)) .|>
+              (xy -> (xy[1], xy[2])) |>
+              filter((x, y) -> 1 <= x && x <= x_max && 1 <= y && y <= y_max, _)
+  return vessels
+end
+
+"For a given tile, generate the image of the distribution given the ground truth data."
+function generateImageTile(ga::GeoArray, df::DataFrame)::Array{Float32,2}
+  vessels = allVessels(ga, df)
   result = zeros(Float32, x_max, y_max)
   for xy in vessels
     result[xy ...] = 1.0
   end
-
-  return result
 end
 
-"TODO: Sample tile by applying given symmetry."
-function sampleTileWithSymmetry(ga::GeoArray)::TyleCollection
-end
+"Sample tile by applying symmetries."
+function sampleTileWithSymmetry(ga::GeoArray, df::DataFrame, n::Int, r::Int)::(TyleCollection, Array{Float32,2})
+  image = generateImageTile(ga, df)
+  transformations = [identity rotations(ga, df, n, r) ...]
+  tiles = [t(ga.A) for t in transformations]
+  return (tiles, image)
 end
 
+struct LocalRotation
+  center::Tuple{Int,Int}
+  r::Int
+  rot::Angle2d
 
+end
+
+"Apply a local rotation. TODO check for out of bounds radii."
+function (rot::LocalRotation)(x::Array{Float32,2})::Array{Float32,2}
+  (c_x, c_y) = rot.center
+  r = rot.r
+  x_window = x[c_x - r:c_x + r, c_y - r:c_y + r]
+  rotation = recenter(rot.rot, center(x_window))
+  warped = no_offset_view(warp(x_window, rotation))
+  (w_x,w_y) = center(warped)
+  y = copy(x)
+  y[c_x - r:c_x + r, c_y - r:c_y + r] = warped[w_x - r:w_x + r, w_y - r:w_y + r]
+  nans = findall(isnan, y)
+  y[nans] = x[nans]
+  return y
+end
+
+"Compute rotations around vessels"
+function rotations(vessels::Array{Tuple{Int,Int},1}, n::Int, r::Float32)::Array{LocalRotation,1}
+  phis = [0:2.0 * pi / n:2 * pi]
+  [LocalRotation(v, r, Angle2d(phi)) for v in vessels for phi in phis]
+end
+end
