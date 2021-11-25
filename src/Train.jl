9639c1fc64e4d7d705317c9367beaa6b5be53e4f
drsk <drsk@lavabit.com>
2021-11-25 09:54:20 +0100

    first working implementation

--- src/Train.jl
+++ src/Train.jl
@@ -10,29 +10,41 @@ using DataFrames
 
 include("Tiles.jl")
 
+u = Unet()
+u = gpu(u)
+opt = Momentum()
+
+const tileSize = 512
+const overlap = 32
+
 function loss(x, y)
     op = clamp.(u(x), 0.001f0, 1.f0)
     mean(bce(op, y))
 end
 
-const TrainData = Vector{Tuple{Matrix{Union{Missing,Float32}},Matrix{Float32},Int, Int}}
+const TrainData = Vector{Tuple{Array{Float32, 4}, Array{Float32, 4}}}
 
-function loadFile(gaFp::String, csvFp)::TrainData
+function loadFile(gaFp::String, csvFp::String)::TrainData
   ga = GeoArrays.read(gaFp)
   csv = CSV.read(csvFp, DataFrame)
-  tiles = Tiles.generateTiles(2048, 64, ga)
+  tiles = Tiles.generateTiles(tileSize, overlap, ga)
   trainData = tiles .|> t -> begin
-                            img = Tiles.generateImageTile(t, csv)
-                            return (t.A[:,:,1], img, 1, 1)
-                          end
+                              img = Tiles.generateImageTile(t, csv)
+                              # TODO replace missing values with min(Float32). Should we do something
+                              # different?
+                              t.A[t.A .|> ismissing] .= typemin(Float32)
+                              return (reshape(t.A[:,:,1], tileSize, tileSize, 1, 1), reshape(img, tileSize, tileSize, 1,1))
+                            end
   return trainData
 end
 
+evalcb = throttle(30) do
+  @show(accuracy())
+  @save "model-checkpoint.bson" model
+end
+
 function unetTrain(data::TrainData)
-    u = Unet()
-    u = gpu(u)
-    opt = Momentum()
-    Flux.train!(loss, Flux.params(u), data, opt)
+    Flux.train!(loss, Flux.params(u), data, opt, cb = evalcb)
 end
 
 end
