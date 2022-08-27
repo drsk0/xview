22e6d7390e93a44bff717840bb206f8d65439489
drsk <drsk@lavabit.com>
2022-08-27 15:42:16 +0200

    moving to Rasters.jl

--- src/Train.jl
+++ src/Train.jl
@@ -4,7 +4,6 @@ export unetTrain, prepareData, readData
 
 using UNet
 using Flux
-using GeoArrays
 using CSV
 using DataFrames
 using CUDA
@@ -15,13 +14,12 @@ using Base.Threads
 using ImageDraw
 using TiledIteration
 using ColorTypes
-
-include("Tiles.jl")
+using Rasters
 
 const TrainData = Tuple{Array{Float32,4},Array{Float32,4}}
 
 function loss(x, y)
-    Flux.Losses.logitbinarycrossentropy(u(x), y, agg = sum)
+    Flux.Losses.binarycrossentropy(clamp.(u(x),0.05f0, 0.95f0), y, agg = sum)
 end
 
 # we keep track of the evolution of the accuracy
@@ -33,86 +31,41 @@ function accuracy(data::Vector{TrainData})
 end
 
 function evalCallback(data)
-    Flux.throttle(30) do
-        @info accuracy(data)
+    Flux.throttle(60) do
+        @info "Mean loss: $(accuracy(data))"
         @save "model-checkpoint.bson" u
     end
 end
 
 function processData(dataDir::String)
     csv = CSV.read(joinpath(dataDir, "train.csv"), DataFrame)
+    dropmissing!(csv, :is_vessel)
     getDataDirs(dataDir) .|> subDir -> processGAs(csv, subDir)
 end
 
 function processGAs(csv::DataFrame, subDir::String)
-    gaV = GeoArrays.read(joinpath(subDir, "VV_dB.tif"))
-    gaH = GeoArrays.read(joinpath(subDir, "VH_dB.tif"))
+    gaV = Raster(joinpath(subDir, "VV_dB.tif"))
+    gaH = Raster(joinpath(subDir, "VH_dB.tif"))
 
-    # TODO get rid of this
-    @threads for ga in [gaV, gaH]
-        ga.A[ismissing.(ga.A)] .= 0
-    end
-
-    gaBat = GeoArrays.read(joinpath(subDir, "bathymetry.tif"))
-    gaBatPrim = Tiles.generateBathymetryTile(gaV, gaBat)
-    img = Tiles.generateImageTile(gaV, csv)
-
-    @threads for (ga, relPath) in
-                 [(gaV, "VV_dB"), (gaH, "VH_dB"), (gaBatPrim, "bathymetry")]
-        gas = reshape(partitionGA(ga), (1, 16))
-        for (i, gaA) in zip(1:16, gas)
-            ga.A = gaA
-            mkpath(joinpath(subDir, "processed"))
-            GeoArrays.write!(gaTileName(subDir, relPath, i), ga)
-        end
-    end
-
-    imgs = reshape(partition(img), (1, 16))
-    for (i, im) in zip(1:16, imgs)
-        mkpath(joinpath(subDir, "processed"))
-        @save imgTileName(subDir, "image", i) im
-    end
-
-
+    gaBat = Raster(joinpath(subDir, "bathymetry.tif"))
+    gaBatPrim = resample(gaBat, to=gaV, method=:near)
+    # TODO also filter for confidence level with in(["MEDIUM", "HIGH"]).(csv.confidence)
+    vessels = @view csv[(csv.scene_id .== subDir) .& (csv.is_vessel .== true), [:detect_scene_row, :detect_scene_column]]
+    img = generateImage(gaV, csv)
+    
+    Rasters.write(joinpath(subDir, "bathymetry_processed.tif"), gaBatPrim)
+    @save imgTileName(subDir, "image.bson") img
 end
 
-# Compute the name of the i-th tile of a GeoArray
-function gaTileName(fp::String, relPath::String, i::Int)::String
-    return joinpath(fp, "processed", relPath * "_$(i).tiff")
-end
-
-function imgTileName(fp::String, relPath::String, i::Int)::String
-    return joinpath(fp, "processed", relPath * "_$(i).bson")
-end
-
-# Paritition a GeoArray in 16 tiles.
-function partitionGA(ga::GeoArray)::Matrix{Array{Union{Missing,Float32},3}}
-    ret = Matrix{Array{Union{Missing,Float32},3}}(undef, (4, 4))
-    (x, y, z) = size(ga)
-    for i = 1:4
-        for j = 1:4
-            ret[i, j] = ga[
-                begin+(i-1)*div(x, 4):begin+i*div(x, 4),
-                begin+(j-1)*div(y, 4):begin+j*div(y, 4),
-                begin:end,
-            ]
-        end
+"For a given tile, generate the image of the distribution given the ground truth data."
+function generateImage(ga::Raster{Float32, 3}, vessels::SubDataFrame)::Raster{Float32,3}
+    result = copy(ga)
+    xs = result.data
+    xs .= 0.0
+    for r in eachrow(vessels)
+        xs[r.detect_scene_row, r.detect_scene_column, 1] = 1.0
     end
-    return ret
-end
-
-function partition(xs::Matrix{T})::Matrix{Matrix{T}} where {T}
-    ret = Matrix{Matrix{T}}(undef, (4, 4))
-    (x, y) = size(xs)
-    for i = 1:4
-        for j = 1:4
-            ret[i, j] = xs[
-                begin+(i-1)*div(x, 4):begin+i*div(x, 4),
-                begin+(j-1)*div(y, 4):begin+j*div(y, 4),
-            ]
-        end
-    end
-    return ret
+    return result
 end
 
 # Return all filepath pointing to a data directory.
@@ -121,36 +74,30 @@ function getDataDirs(dataDir::String)::Vector{String}
           filter(fp -> isdir(fp) && !endswith(fp, "shoreline"), _)
 end
 
-function selectTile(ga::GeoArray, i::Int, j::Int)::Array{Union{Missing,Float32},3}
-    return @view ga.A[1+i*tileSize:(i+1)*tileSize, 1+j*tileSize:(j+1)*tileSize, begin:end]
-end
-
-function selectImg(img::Matrix{Float32}, i::Int, j::Int)::Matrix{Float32}
-    return @view img[1+i*tileSize:(i+1)*tileSize, 1+j*tileSize:(j+1)*tileSize]
-end
-
-function drawBoxes(ga::GeoArray, img::Matrix{Float32})::GeoArray
-    c = maximum(ga.A[:, :, 1]) + 20.0f0
-    ret = Gray.(ga.A[:, :, 1])
-    for (x, y) in [(i[1], i[2]) for i = CartesianIndices(ret) if ret[Tuple(i)...] > 0.7]
+function drawBoxes(ga::Raster{Float32, 3}, img::Raster{Float32, 3})::Raster{Float32, 3}
+    tmp = copy(ga)
+    xs = ga.data
+    ys = img.data
+    c = maximum(xs) + 20.0f0
+    ret = Gray.(xs[:, :, 1])
+    for (x, y) in [(i[1], i[2]) for i = CartesianIndices(ret) if ys[Tuple(i)...] > 0.9]
         draw!(
             ret,
-            Polygon(RectanglePoints(Point(x - 10, y - 10), Point(x + 10, y + 10))),
+            Ellipse(CirclePointRadius(x, y, 20; fill = true)),
             Gray(c),
         )
     end
-    ret = GeoArray(gray.(ret))
-    ret.f = ga.f
-    ret.crs = ga.crs
-    return ret
+    tmp.data[:,:,1] = gray.(ret)
+
+    return tmp
 end
 
 function applyU(
     u::Unet,
     tileSize::Int,
-    gaV::GeoArray,
-    gaH::GeoArray,
-    bat::GeoArray,
+    gaV::Raster{Float32, 3},
+    gaH::Raster{Float32, 3},
+    bat::Raster{Float32, 3},
 )::Matrix{Float32}
     tiles = TileIterator(axes(gaV.A[:, :, 1]), RelaxStride((tileSize, tileSize)))
     (x, y, z) = size(gaV)
@@ -205,7 +152,7 @@ function unetTrain(dataDir::String, batchSize::Int, tileSize::Int)
                     push!(data, (ret, im))
                 else
                     epochCounter = epochCounter + 1
-                    println("starting training epoch $(epochCounter)")
+                    @info "starting training epoch $(epochCounter)"
                     Flux.train!(loss, Flux.params(u), data, opt, cb = evalCallback(data))
                     empty!(data)
                 end
