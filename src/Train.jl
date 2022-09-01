df3287078511e5e7b418cf997e5b934b16c50003
drsk <drsk@lavabit.com>
2022-09-01 11:04:45 +0200

    updated drawBox, applyU

--- src/Train.jl
+++ src/Train.jl
@@ -6,20 +6,21 @@ using UNet
 using Flux
 using CSV
 using DataFrames
+using JLD2: save_object, load_object
 using CUDA
-using BSON: @save, @load
 using Pipe
 using Base.Iterators
 using Base.Threads
 using TiledIteration
 using Rasters
 using Plots
+using Base.Iterators: zip, partition
 import DimensionalData.Dimensions.LookupArrays as DD
 
 const TrainData = Tuple{Array{Float32,4},Array{Float32,4}}
 
 function loss(x, y)
-    Flux.Losses.binarycrossentropy(clamp.(u(x),0.05f0, 0.95f0), y, agg = sum)
+    Flux.Losses.binarycrossentropy(clamp.(u(x), 0.05f0, 0.95f0), y, agg = sum)
 end
 
 # we keep track of the evolution of the accuracy
@@ -33,38 +34,47 @@ end
 function evalCallback(data)
     Flux.throttle(60) do
         @info "Mean loss: $(accuracy(data))"
-        @save "model-checkpoint.bson" u
+        save_object("model-checkpoint.jld2", u)
     end
 end
 
-function processData(dataDir::String)
+function preprocessData(dataDir::String, tileSize::Int)
     csv = CSV.read(joinpath(dataDir, "train.csv"), DataFrame)
     dropmissing!(csv, :is_vessel)
-    getDataDirs(dataDir) .|> subDir -> processGAs(csv, subDir)
+    for subDir in getDataDirs(dataDir)
+        preprocessDir(subDir, csv, tileSize)
+    end 
 end
 
-function processGAs(csv::DataFrame, subDir::String)
-    gaV = Raster(joinpath(subDir, "VV_dB.tif"))
-    gaH = Raster(joinpath(subDir, "VH_dB.tif"))
-
-    gaBat = Raster(joinpath(subDir, "bathymetry.tif"))
-    gaBatPrim = resample(gaBat, to=gaV, method=:near)
+function preprocessDir(subDir::String, csv::DataFrame, tileSize::Int)
+    @info "Processing " * subDir
+    id = last(splitpath(subDir))
     # TODO also filter for confidence level with in(["MEDIUM", "HIGH"]).(csv.confidence)
-    vessels = @view csv[(csv.scene_id .== subDir) .& (csv.is_vessel .== true), [:detect_scene_row, :detect_scene_column]]
-    img = generateImage(gaV, csv)
-    
-    Rasters.write(joinpath(subDir, "bathymetry_processed.tif"), gaBatPrim)
-    @save imgTileName(subDir, "image.bson") img
+    vessels = @view csv[
+        (csv.scene_id.==id).&(csv.is_vessel.==true),
+        [:detect_scene_row, :detect_scene_column],
+    ]
+    open(Raster(joinpath(subDir, "VV_dB.tif"))) do gaV
+        img = generateImage(gaV, vessels)
+        Rasters.write(joinpath(subDir, "image.tif"), img)
+        
+        open(Raster(joinpath(subDir, "bathymetry.tif"))) do gaBat
+            gaBatPrim = resample(gaBat, to = gaV, method = :near)
+            gaV = nothing
+            Rasters.write(joinpath(subDir, "bathymetry_processed.tif"), gaBatPrim)
+        end
+    end
+    ts = partitionTiles(subDir, tileSize)
+    save_object("tiles_$(tileSize).jld2", ts)
 end
 
 "For a given tile, generate the image of the distribution given the ground truth data."
-function generateImage(ga::Raster{Float32, 3}, vessels::SubDataFrame)::Raster{Float32,3}
-    result = copy(ga)
-    xs = result.data
-    xs .= 0.0
-    for r in eachrow(vessels)
-        xs[r.detect_scene_column, r.detect_scene_row, 1] = 1.0
-    end
+function generateImage(ga::Raster{Float32,3}, vessels::SubDataFrame)::Raster{Float32,3}
+    result = Raster(zeros(Float32, dims(ga)))
+    vs = [
+        CartesianIndex(v.detect_scene_column, v.detect_scene_row) for v in eachrow(vessels)
+    ]
+    result[vs] .= 1.0
     return result
 end
 
@@ -74,37 +84,65 @@ function getDataDirs(dataDir::String)::Vector{String}
           filter(fp -> isdir(fp) && !endswith(fp, "shoreline"), _)
 end
 
-function drawBoxes(ga::Raster{Float32, 3}, img::Raster{Float32, 3}, treeshold::Float32)::Plots.Plot
+# Plot a raster together with the marked vessel positions.
+function drawBoxes(ga::Raster{Float32,3}, vessels::Vector{CartesianIndex{2}}, predicted_vessels::Vector{CartesianIndex{2}} = [])::Plots.Plot
     x_dim, y_dim = dims(ga)[1], dims(ga)[2]
-    x_sign = DD.order(x_dim) isa DD.ForwardOrdered ? 1 : -1 
-    y_sign = DD.order(y_dim) isa DD.ForwardOrdered ? 1 : -1 
+    x_sign = DD.order(x_dim) isa DD.ForwardOrdered ? 1 : -1
+    y_sign = DD.order(y_dim) isa DD.ForwardOrdered ? 1 : -1
     (xbounds, ybounds, zbounds) = bounds(ga)
     xbound = DD.order(x_dim) isa DD.ForwardOrdered ? xbounds[1] : xbounds[2]
     ybound = DD.order(y_dim) isa DD.ForwardOrdered ? ybounds[1] : ybounds[2]
     (xsize, ysize, zsize) = size(ga)
     xfactor = (xbounds[2] - xbounds[1]) / xsize
     yfactor = (ybounds[2] - ybounds[1]) / ysize
-    vessels = findall(x -> x > treeshold, img[:,:,1])
-    vessels_cords = [(x_sign * xfactor, y_sign * yfactor) .* (v.I[1], v.I[2]) .+ (xbound, ybound) for v in vessels]
+    vessels_coords = [
+        (x_sign * xfactor, y_sign * yfactor) .* (v.I[1], v.I[2]) .+ (xbound, ybound) for
+        v in vessels
+    ]
     p = plot(ga)
-    scatter!(p, vessels_cords; legend=:none, markercolor=:black, markerstrokewidth=10, markerstrokealpha=1.0, markershape=:rect, markeralpha=0.6)
+    scatter!(
+        p,
+        vessels_coords;
+        legend = :none,
+        markercolor = :black,
+        markerstrokewidth = 10,
+        markerstrokealpha = 1.0,
+        markershape = :rect,
+        markeralpha = 0.6,
+    )
+    if !isempty(predicted_vessels)
+        predicted_vessels_coords = [
+            (x_sign * xfactor, y_sign * yfactor) .* (v.I[1], v.I[2]) .+ (xbound, ybound) for
+            v in predicted_vessels
+        ]
+        scatter!(
+            p,
+            predicted_vessels_coords;
+            legend = :none,
+            markercolor = :red,
+            markerstrokewidth = 10,
+            markerstrokealpha = 1.0,
+            markershape = :rect,
+            markeralpha = 0.6,
+        )
+    end
     return p
 end
 
+# Apply a Unet, returning all pixel coordinates above a given threshold.
 function applyU(
     u::Unet,
+    rs::RasterStack,
     tileSize::Int,
-    gaV::Raster{Float32, 3},
-    gaH::Raster{Float32, 3},
-    bat::Raster{Float32, 3},
-)::Matrix{Float32}
-    tiles = TileIterator(axes(gaV.A[:, :, 1]), RelaxStride((tileSize, tileSize)))
-    (x, y, z) = size(gaV)
-    ret = Matrix{Float32}(undef, x, y)
+    threshold::Float32
+)::Vector{CartesianIndex}
+    tiles = TileIterator(axes(rs[:, :, 1]), RelaxStride((tileSize, tileSize)))
+    (x, y, z) = size(rs[:VV_dB].data)
+    img = Matrix{Float32}(undef, x, y)
     for t in tiles
-        ret[t...] = u(
+        img[t...] = u(
             reshape(
-                hcat(gaV.A[t..., 1], gaH.A[t..., 1], bat.A[t..., 1]),
+                hcat(rs[:VV_dB].data[t..., 1], rs[:VH_dB].data[t..., 1], rs[:bathymetry_processed].data[t..., 1]),
                 tileSize,
                 tileSize,
                 3,
@@ -112,54 +150,90 @@ function applyU(
             ),
         )
     end
-    return ret
+    return findall(x -> x > threshold, img)
 end
 
-u = Unet(3, 1)
-function unetTrain(dataDir::String, batchSize::Int, tileSize::Int)
+mutable struct Tiles
+    empty::Vector{Tuple{UnitRange{Int}, UnitRange{Int}}}
+    nonempty::Vector{Tuple{UnitRange{Int}, UnitRange{Int}}}
+end
+
+function Tiles()::Tiles
+    Tiles([], [])
+end
+
+function partitionTiles(fp::String, tileSize::Int)::Tiles
+    tilesPerThread = repeat([Tiles()], Threads.nthreads())
+    rs = RasterStack(
+        (
+            x -> joinpath(fp, x)
+        ).(["VV_dB.tif", "VH_dB.tif", "bathymetry_processed.tif", "image.tif"]);
+        lazy = true,
+    )
+    tiles = TileIterator(axes(@view rs[:, :, 1]), RelaxStride((tileSize, tileSize)))
+    @threads for j in tiles
+        tId = Threads.threadid()
+        ts = tilesPerThread[tId]
+        t = @view rs[j..., 1]
+        if all(t[:image] .== 0)
+            @info "Empty tile $(j)"
+            push!(ts.empty, j)
+        else
+            @info "Non-empty tile $(j)"
+            push!(ts.nonempty, j)
+        end
+    end
+    
+    res = Tiles()
+    for ts in tilesPerThread
+        append!(res.empty, ts.empty)
+        append!(res.nonempty, ts.nonempty)
+    end
+    
+    return res
+end
+
+u = gpu(Unet(3, 1))
+function unetTrain(dataDir::String, batchSize::Int, nonemptyNr::Int, tileSize::Int)
+    @assert nonemptyNr < batchSize
+
     opt = Momentum()
 
     dataDirs = getDataDirs(dataDir)
-    @info dataDirs
+    @info "Training with data directories $(dataDirs)"
 
-    data = Vector{TrainData}(undef, 0)
     epochCounter = 0
 
     for fp in dataDirs
-        for i = 1:16
-
-            gaV = GeoArrays.read(gaTileName(fp, "VV_dB", i))
-            gaH = GeoArrays.read(gaTileName(fp, "VH_dB", i))
-            gaBat = GeoArrays.read(gaTileName(fp, "bathymetry", i))
-            @load imgTileName(fp, "image", i) im
-            img = im
-            tiles = TileIterator(axes(gaV.A[:, :, 1]), RelaxStride((tileSize, tileSize)))
-            for j in tiles
-                tV = gaV[j..., 1]
-                tH = gaH[j..., 1]
-                tBat = gaBat[j..., 1]
-                im = reshape(img[j...], tileSize, tileSize, 1, 1)
-                if count(x -> x == 1.0, im) == 0
-                    continue;
-                end
+        rs = RasterStack(
+            (
+                x -> joinpath(fp, x)
+            ).(["VV_dB.tif", "VH_dB.tif", "bathymetry_processed.tif", "image.tif"]);
+            lazy = true,
+        )
+        tiles = load_object(joinpath(fp, "tiles_$(tileSize).jld2"))
+        emptyTs = partition(tiles.empty, batchSize - nonemptyNr)
+        nonemptyTs = partition(tiles.nonempty, nonemptyNr)
+        tsZipped = zip(emptyTs, nonemptyTs)
+        for (tsEmpty, tsNonEmpty) in tsZipped
+            data = TrainData[]
+            ts = append!(collect(tsNonEmpty), collect(tsEmpty))
+            for (tx, ty) in ts
                 ret = zeros(Float32, tileSize, tileSize, 3, 1)
-                ret[:, :, 1, 1] = tV[:, :]
-                ret[:, :, 2, 1] = tH[:, :]
-                ret[:, :, 3, 1] = tBat[:, :]
-
-                if (length(data) < batchSize)
-                    push!(data, (ret, im))
-                else
-                    epochCounter = epochCounter + 1
-                    @info "starting training epoch $(epochCounter)"
-                    Flux.train!(loss, Flux.params(u), data, opt, cb = evalCallback(data))
-                    empty!(data)
-                end
+                ret[:, :, 1, 1] = rs[:VV_dB].data[tx, ty, 1] 
+                ret[:, :, 2, 1] = rs[:VH_dB].data[tx, ty, 1]
+                ret[:, :, 3, 1] = rs[:bathymetry_processed].data[tx, ty, 1]
+                img = reshape(rs[:image].data[tx, ty, 1], tileSize, tileSize, 1, 1)
+                push!(data, (ret, img))
             end
 
-
+            epochCounter = epochCounter + 1
+            @info "starting training epoch $(epochCounter)"
+            Flux.train!(loss, Flux.params(u), data, opt, cb = evalCallback(data))
+            empty!(data)
         end
-
     end
+
 end
-end
+
+end #Train
